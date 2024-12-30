bfb7b9ddec1caf1359b86a479b9fe40aaa2ae971
dhiraj19999 <dhirajgarad46@gmail.com>
2024-12-30 09:03:36 -0800

    frontend backend setup, tailwind,clerk integration completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+     
+    </>
+  )
+}
+
+export default App
