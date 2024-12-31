f53800954946a42d0caa39b57039acc09a3fb328
dhiraj19999 <dhirajgarad46@gmail.com>
2024-12-31 07:45:51 -0800

    admin-auth and admin-controller completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
+import fileUpload from 'express-fileupload'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import adminRoutes from './routes/admin.route.js'
@@ -7,17 +9,24 @@ import songRoutes from './routes/song.route.js'
 import albumRoutes from './routes/album.route.js'
 import statRoutes from './routes/stat.route.js'
 import { connectDB } from './lib/db.js'
+import { clerkMiddleware } from '@clerk/express'
 dotenv.config()
 const app=express()
-
+const __dirname=path.resolve()
 const PORT=process.env.PORT
 app.use(express.json())// to parse req.body
+app.use(clerkMiddleware()) // this will add auth to req obj ==> req.auth.userid =>whenever any url hit auth get added to req object
+app.use(fileUpload({useTempFiles:true,tempFileDir:path.join(__dirname,"tmp"),createParentPath:true,limits:{fileSize:10*1024*1024}}))
 app.use("/api/users",userRoutes);
 app.use("/api/auth",authRoutes);
 app.use("/api/admin",adminRoutes);
 app.use("/api/songs",songRoutes);
 app.use("/api/albums",albumRoutes);
 app.use("/api/stats",statRoutes);
+// error handler
+app.use((err, req, res, next) => {
+	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
+});
 
 app.listen(PORT,()=>{
 console.log(`PORT RUNNING ON ${PORT}`)
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
-
-
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { requireAdmin } from "../middleware/auth.middleware.js"
+import { createSong } from "../controller/admin.controller.js";
+import { deleteSong } from "../controller/admin.controller.js";
+import { deleteAlbum } from "../controller/admin.controller.js";
+import { createAlbum } from "../controller/admin.controller.js";
+import { checkAdmin } from "../controller/admin.controller.js";
 const router=Router()
+router.use(protectRoute, requireAdmin);
 
-router.get("/",(req,res)=>{
-    res.send("admin route with get method")
-})
+router.get("/check", checkAdmin);
 
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router
